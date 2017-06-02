c5cdc51bb85070cdd08a0671d2aef5d339ac49ea
jinyan.cao <caojinyanivy@163.com>
2017-06-02 08:08:02 +0800

    +页面显示+日志

--- client/src/main/java/swa/annotation/BeanInitProcessor.java
+++ client/src/main/java/swa/annotation/BeanInitProcessor.java
@@ -2,38 +2,46 @@ package swa.annotation;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import swa.annotation.ValueSetter;
 import swa.service.CallBackLoader;
 import swa.service.DataStorer;
 import swa.service.DataUpdater;
 import swa.service.ListenerConfig;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 /**
  * Created by jinyan on 5/25/17.
  */
 public class BeanInitProcessor implements BeanPostProcessor {
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        return null;
+        return bean;
     }
 
     public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
-        Field[] fields = bean.getClass().getFields();//只处理属性上的注解
+        System.out.println("postProcessAfterInitialization begin:" + beanName);
+        Field[] fields = bean.getClass().getDeclaredFields();//只处理属性上的注解
         for (final Field field : fields) {
             if (field.getAnnotation(ValueSetter.class) != null) {
                 DataUpdater.addListener(new ListenerConfig("filename.properties", new CallBackLoader() {
                     public void loadData() {
-                        try {
-                            System.out.println("postProcessAfterInitialization" + beanName);
-                            field.set(bean, DataStorer.getValue(field.getAnnotation(ValueSetter.class).value()));
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        }
+                        setField(field, bean, DataStorer.getValue(field.getAnnotation(ValueSetter.class).value()));
                     }
                 }));
+                setField(field, bean, DataStorer.getValue(field.getAnnotation(ValueSetter.class).value()));
             }
         }
         return bean;
+    }
+
+    private void setField(Field field, Object bean, Map<String, String> map) {
+        try {
+            System.out.println("set field:" + field.getName() + "#value:" + map);
+            field.setAccessible(true);
+            field.set(bean, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
     }
 }
